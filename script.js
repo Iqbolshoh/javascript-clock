7a83cf02d1f43eab4dbd22ecdb43e36ef500b84a
Iqbolshoh <148738947+Iqbolshoh@users.noreply.github.com>
2024-08-09 18:22:15 +0500

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let hrs = document.getElementById("hour");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+setInterval(() => {
+    let currentTime = new Date();
+    hrs.innerHTML = (currentTime.getHours() < 10 ? "0" : "")
+        + currentTime.getHours();
+    min.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "")
+        + currentTime.getMinutes();
+    sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "")
+        + currentTime.getSeconds();
+})
